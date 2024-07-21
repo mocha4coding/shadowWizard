59ff277f6e4c9a34b1901b36da92288c535561bc
mocha4coding <manisini.chakraborty@gmail.com>
2024-07-21 11:02:49 +0530

    Added parallax and some tweaks
    Added parallax and some tweaks

--- scripts/lamp.gd
+++ scripts/lamp.gd
@@ -4,3 +4,11 @@ extends Node2D
 func _physics_process(delta):
 	# Add the gravity.
 	look_at(get_global_mouse_position())
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	print(body.name)
