fb0b47c4758ed0e856b56ff4bf3e939ca0ec381f
Manisini <manisini.chakraborty@gmail.com>
2024-07-29 23:08:37 +0530

    Added  fire bridge

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var collect_mushroom_instruction = $CollectMushroomInstruction
 @onready var collect_mushroom_ui_board_instruction = $LevelOneUI/CollectMushroomUiBoardInstruction
 @onready var space_bar_for_magic_light_instruction = $SpaceForMagicLightInstructionArea2D/SpaceBarForMagicLightInstruction
+@onready var push_shadow_instruction = $PushShadowInstruction
+@onready var object_original = $Object_original
+
 
 @onready var golden_mushroom_tutorial = $GoldenMushroomTutorial
 # Called when the node enters the scene tree for the first time.
@@ -36,10 +39,10 @@ func _on_area_2d_body_entered(body):
 func _on_space_for_magic_light_instruction_area_2d_body_entered(body):
 	if body is Player:
 		space_bar_for_magic_light_instruction.show()
+		push_shadow_instruction.show()
 
 
 func _on_space_for_magic_light_instruction_area_2d_body_exited(body):
 	if body is Player:
-		
 		$SpaceForMagicLightInstructionArea2D.queue_free()
-
+		
